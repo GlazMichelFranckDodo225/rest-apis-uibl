1925278eeadc2059c4983ee14b2ffe1ed3de7e64
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-12-01 09:40:13 +0100

    Project Initialization

---
+++ src/main/java/com/dgmf/restapisuibl/RestApisUiblApplication.java
@@ -0,0 +1,13 @@
+package com.dgmf.restapisuibl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApisUiblApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApisUiblApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgmf/restapisuibl/RestApisUiblApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.restapisuibl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApisUiblApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
