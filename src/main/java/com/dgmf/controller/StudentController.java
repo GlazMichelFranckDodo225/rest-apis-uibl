e820a9e35a05308497888bb6a31429638f8a4cab
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-12-02 08:26:53 +0100

    @Value Annotation to Read Property

--- src/main/java/com/dgmf/controller/StudentController.java
+++ src/main/java/com/dgmf/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.dgmf.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/students")
 public class StudentController {
+    @Value("${app.name:Default Demo App}")
+    private String appName;
+
     @GetMapping
     public String getStudents() {
-        return "Student List";
+        // return "Student List";
+        return appName;
     }
 }
